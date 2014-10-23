fc8e25bbb2a64bdf0964d1d5d493c44ef3916d14
hairshmara <harish.mara@gmail.com>
2014-10-22 20:32:38 -0400

    test

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Uses library MASS for inverse function
 
-## Write a short comment describing this function
+## Creates a special matrix that has access functions to get the matrix and the cached inverse
+## The set function also sets the cache to NULL
+library("MASS")
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    minv <- NULL
+    x <<- matrix(...)
+    set <- function(y) {
+        minv<<-NULL
+        x<<-y
+    }
+    get <- function() x
+    getinv <- function() minv
+    setinv <- function(y) minv <<- y
+    list(set = set, get = get, 
+         getinv = getinv, setinv = setinv)
 }
 
 
-## Write a short comment describing this function
-
+# Obtains the cached inverse of the special matrix created by makeCacheMatrix
+# If the inverse is not null, then it is returned, else calculates the inverse
+# and sets it into the cache for subsequents calls.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    linv <- x$getinv()
+    if (!is.null(linv)) {
+        message ("inverse returned from cache")
+        return (linv)
+    }
+    linv <- ginv(x$get())
+    x$setinv (linv)
+    linv
 }
